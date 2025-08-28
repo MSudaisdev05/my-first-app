17cea793529881f229b153ff2e945a6b0e81bb0d
Muhammad Sudais <msudais.dev05@gmail.com>
2025-08-28 01:27:26 -0700

    costem navigation bar

--- lib/ui/root/root_scrreen.dart
+++ lib/ui/root/root_scrreen.dart
@@ -1,3 +1,7 @@
+import 'package:first_app/core/constatns/colors.dart';
+import 'package:first_app/core/constatns/strings.dart';
+import 'package:first_app/core/constatns/text_style.dart';
+import 'package:first_app/ui/screens/auth/Setting.dart';
 import 'package:first_app/ui/screens/auth/homescreen.dart';
 import 'package:flutter/material.dart';
 
@@ -10,30 +14,134 @@ class RootScreen extends StatefulWidget {
 
 class _RootScreenState extends State<RootScreen> {
   int selectedIndex = 0;
-  List<Widget> screen = [Homescreen(), Text("Serch"), Text("setting")];
+  onClick(value) {
+    selectedIndex = value;
+  }
+
+  List<Widget> screen = [
+    Homescreen(),
+    Center(child: Text("Serch", style: style42B)),
+    Setting(),
+    Center(child: Text("dashbord", style: style42B)),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: screen[selectedIndex],
+      bottomNavigationBar: Container(
+        height: 70,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: blackColor.withOpacity(0.25),
+              offset: Offset(0, 3),
+              blurRadius: 4,
+              spreadRadius: 6,
+            ),
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  onClick(0);
+                });
+              },
+              child: CircleAvatar(
+                radius: 20,
+                backgroundColor:
+                    selectedIndex == 0 ? primeryColor : Colors.transparent,
+                child: Image.asset(
+                  "$iconsAssets/botm2.png",
+                  color: selectedIndex == 0 ? Colors.white : gryColor,
+                  scale: 1,
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  onClick(1);
+                });
+              },
+              child: CircleAvatar(
+                radius: 20,
+                backgroundColor:
+                    selectedIndex == 1 ? primeryColor : Colors.transparent,
+                // child: Image.asset(
+                //   "$iconsAssets/botm2.png",
+                // color: selectedIndex == 1 ? Colors.white : gryColor,
+                // scale: 3,
+                child: Icon(
+                  Icons.home,
+                  color: selectedIndex == 1 ? whiteColor : gryColor,
+                ),
+              ),
+            ),
 
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: selectedIndex,
-        selectedItemColor: Colors.blueAccent,
-        unselectedItemColor: Colors.blueGrey,
-        onTap: (value) {
-          setState(() {
-            selectedIndex = value;
-          });
-        },
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: "serch"),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: "Seitting",
-          ),
-        ],
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  onClick(2);
+                });
+              },
+              child: CircleAvatar(
+                radius: 20,
+                backgroundColor:
+                    selectedIndex == 2 ? primeryColor : Colors.transparent,
+                child: Image.asset(
+                  "$iconsAssets/botm2.png",
+                  color: selectedIndex == 2 ? Colors.white : gryColor,
+                  scale: 3,
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  onClick(3);
+                });
+              },
+              child: CircleAvatar(
+                radius: 20,
+                backgroundColor:
+                    selectedIndex == 3 ? primeryColor : Colors.transparent,
+                child: Image.asset(
+                  "$iconsAssets/botm2.png",
+                  color: selectedIndex == 3 ? Colors.white : gryColor,
+                  scale: 3,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
+
+      // bottomNavigationBar: BottomNavigationBar(
+      //   currentIndex: selectedIndex,
+      //   selectedItemColor: Colors.red,
+      //   unselectedItemColor: Colors.blueGrey,
+      //   onTap: (value) {
+      //     setState(() {
+      //       selectedIndex = value;
+      //     });
+      //   },
+      //   items: [
+      //     // BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+      //     BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+
+      //     // BottomNavigationBarItem(icon: Icon(Icons.search), label: "serch"),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.dashboard_outlined),
+      //       label: "",
+      //     ),
+      //     BottomNavigationBarItem(icon: Icon(Icons.shopping_bag), label: ""),
+      //     BottomNavigationBarItem(icon: Icon(Icons.shopping_bag), label: ""),
+      //   ],
+      // ),
     );
   }
 }
--- lib/ui/screens/auth/Setting.dart
+++ lib/ui/screens/auth/Setting.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
+
 class Setting extends StatelessWidget {
   const Setting({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text(""),
+      body: Center(
+        child: Container(
+          height: 500,
+          width: 500,
+          decoration: BoxDecoration(color: Colors.greenAccent),
+
+          child: Text(
+            "This is seitting page ",
+            style: TextStyle(color: Colors.red, fontSize: 42),
+          ),
+        ),
+      ),
     );
   }
 }
